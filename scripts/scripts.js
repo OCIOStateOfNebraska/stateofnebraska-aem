2ae21467696fad8aea3a5ef018cac9fc2b53a024
Kevin Harris <kharris@reingold.com>
2025-02-28 16:30:08 -0500

    SNE-34 initial uswds set up and site banner block

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,9 @@
 import {
+  decorateBlock,
   buildBlock,
+  loadBlock,
   loadHeader,
   loadFooter,
-  decorateButtons,
   decorateIcons,
   decorateSections,
   decorateBlocks,
@@ -22,9 +23,9 @@ function buildHeroBlock(main) {
   const picture = main.querySelector('picture');
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
-    const section = document.createElement('div');
-    section.append(buildBlock('hero', { elems: [picture, h1] }));
-    main.prepend(section);
+    // const section = document.createElement('div');
+    // section.append(buildBlock('hero', { elems: [picture, h1] }));
+    // main.prepend(section);
   }
 }
 
@@ -40,6 +41,16 @@ async function loadFonts() {
   }
 }
 
+// Add USWDS Banner to page
+function loadBanner(body) {
+  const bannerWrapper = document.createElement('div');
+  const bannerBlock = buildBlock('banner', '');
+  body.prepend(bannerWrapper);
+  bannerWrapper.append(bannerBlock);
+  decorateBlock(bannerBlock);
+  return loadBlock(bannerBlock);
+}
+
 /**
  * Builds all synthetic blocks in a container element.
  * @param {Element} main The container element
@@ -51,6 +62,44 @@ function buildAutoBlocks(main) {
     // eslint-disable-next-line no-console
     console.error('Auto Blocking failed', error);
   }
+}
+
+/**
+ * Decorates paragraphs containing a single link as buttons.
+ * @param {Element} element container element
+ */
+function decorateButtons(element) {
+  element.querySelectorAll('a').forEach((a) => {
+    a.title = a.title || a.textContent;
+    if (a.href !== a.textContent) {
+      const up = a.parentElement;
+      const twoup = a.parentElement.parentElement;
+      if (!a.querySelector('img')) {
+        if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV')) {
+          a.className = 'usa-button'; // default
+          up.classList.add('button-container');
+        }
+        if (
+          up.childNodes.length === 1
+          && up.tagName === 'STRONG'
+          && twoup.childNodes.length === 1
+          && twoup.tagName === 'P'
+        ) {
+          a.className = 'usa-button usa-button--secondary';
+          twoup.classList.add('button-container');
+        }
+        if (
+          up.childNodes.length === 1
+          && up.tagName === 'EM'
+          && twoup.childNodes.length === 1
+          && twoup.tagName === 'P'
+        ) {
+          a.className = 'usa-button usa-button--outline';
+          twoup.classList.add('button-container');
+        }
+      }
+    }
+  });
 }
 
 /**
@@ -74,6 +123,7 @@ export function decorateMain(main) {
 async function loadEager(doc) {
   document.documentElement.lang = 'en';
   decorateTemplateAndTheme();
+  loadBanner(doc.querySelector('body'));
   const main = doc.querySelector('main');
   if (main) {
     decorateMain(main);
@@ -127,3 +177,32 @@ async function loadPage() {
 }
 
 loadPage();
+
+// add uswds js to page
+(function uswdsInit() {
+  const loadingClass = 'usa-js-loading';
+  let fallback = '';
+
+  document.documentElement.classList.add(loadingClass);
+  function revertClass() {
+    document.documentElement.classList.remove(loadingClass);
+  }
+
+  fallback = setTimeout(revertClass, 8000);
+
+  function verifyLoaded() {
+    if (window.uswdsPresent) {
+      clearTimeout(fallback);
+      revertClass();
+      window.removeEventListener('load', verifyLoaded, true);
+    }
+  }
+
+  window.addEventListener('load', verifyLoaded, true);
+}());
+
+const uswds = document.createElement('script');
+const body = document.querySelector('body');
+uswds.async = 'true';
+uswds.src = '/scripts/uswds.min.js';
+body.append(uswds);
