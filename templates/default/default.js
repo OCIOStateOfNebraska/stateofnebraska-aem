9cd08900245f25100eb2cb629c9a7b9c250a155d
Kevin Harris <kharris@reingold.com>
2025-03-27 09:53:53 -0400

    SNE-74 adjusting template decoration

--- templates/default/default.js
+++ templates/default/default.js
@@ -1,31 +1,24 @@
+import {
+	div
+} from '../../scripts/dom-helpers.js';
+
+/**
+ * Decorates the document to align with USWDS Documentation Page Template
+ * @param {Element} doc current document
+ */
 export default async function decorate( doc ) {
-	// const h1 = doc.querySelector('h1');
-	// await buildPostData(h1);
-
-	// const classes = ['section'];
-	// const sidebarSection = createElement('div', classes, {
-	//   'data-section-status': 'initialized',
-	// });
-	// const sidebarContainer = createElement('div');
-	// sidebarSection.append(sidebarContainer);
-
-	// let sidebarPreviousSection;
-	// let sectionFound = false;
-	// const sections = [...doc.querySelectorAll('.section')];
-	// while (!sectionFound && sections.length > 0) {
-	//   const section = sections.pop();
-	//   if (!sidebarPreviousSection) {
-	//     sidebarPreviousSection = section;
-	//   } else if (section.classList.contains('related-content-container') || section.classList.contains('post-cards-container')) {
-	//     sidebarPreviousSection = section;
-	//   } else {
-	//     sectionFound = true;
-	//   }
-	// }
-	// const postSidebar = buildBlock('post-sidebar', '');
-	// sidebarContainer.append(postSidebar);
-	// sidebarPreviousSection.insertAdjacentElement('beforebegin', sidebarSection);
-	// decorateBlock(postSidebar);
+	// TODO: see if any of this is common between USWDS templates and should be consolidated
 	const main = doc.querySelector( 'main' );
 	main.classList.add( 'desktop:grid-col-9', 'usa-prose' );
+	const usaSectionDiv = div( { class: 'usa-section' } );
+	const usaGridDiv = div( { class: 'grid-container' } );
+	const usaGridRowDiv = div( { class: 'grid-row grid-gap' } );
+	usaGridRowDiv.append( main );
+	usaGridDiv.append( usaGridRowDiv );
+	usaSectionDiv.append( usaGridDiv );
+	document.querySelector( 'header' ).after( usaSectionDiv );
+	// TODO: create / decorate sidenav
+	// const siteNav = buildBlock('sidnav', '');
+	// usaGridRowDiv.append(siteNav);
+	// decorateBlock(siteNav);
 }
