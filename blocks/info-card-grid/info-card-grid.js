79bd5201376bc24253a42c6b335c1e33462f8765
Carly Wooten <cwooten@reingold.com>
2025-04-07 11:43:48 -0700

    SNE-53 LCP issues

--- blocks/info-card-grid/info-card-grid.js
+++ blocks/info-card-grid/info-card-grid.js
@@ -109,7 +109,13 @@ export default function decorate( block ) {
 		ul.append( li );
 	} );
 
-	ul.querySelectorAll( 'picture > img' ).forEach( ( img ) => img.closest( 'picture' ).replaceWith( createOptimizedPicture( img.src, img.alt, false, [{ width: '750' }] ) ) );
+	ul.querySelectorAll( 'picture > img' ).forEach( ( img ) => img.closest( 'picture' ).replaceWith( createOptimizedPicture( img.src, img.alt, false, [{ width: '605' }] ) ) );
+	ul.querySelectorAll( 'picture > img' ).forEach( ( img ) => {
+		img.setAttribute( 'height', '340' );
+		img.setAttribute( 'width', '605' );
+		img.setAttribute( 'loading', 'lazy' );
+	});
+
 	block.textContent = '';
 	block.append( ul );
 }
