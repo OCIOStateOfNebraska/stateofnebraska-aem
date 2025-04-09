83c658829451d90b8fde47ee9970862e75e2a23c
sbommakanti <sbommakanti@reingold.com>
2025-04-09 09:42:56 -0400

    changes for 81 reverted accordion js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,19 +1,36 @@
 import { domEl } from '../../scripts/dom-helpers.js';
 import { createId } from '../../scripts/utils.js';
-export default function decorate(block) {
-  [...block.children].forEach((row) => {
-    // decorate accordion item label
-    const label = row.children[0];
-    const summary = document.createElement('summary');
-    summary.className = 'accordion-item-label';
-    summary.append(...label.childNodes);
-    // decorate accordion item body
-    const body = row.children[1];
-    body.className = 'accordion-item-body';
-    // decorate accordion item
-    const details = document.createElement('details');
-    details.className = 'accordion-item';
-    details.append(summary, body);
-    row.replaceWith(details);
-  });
+
+export default function decorate( block ) {
+	let accordions = block.children;
+	let usaAccordion = domEl( 'div', { class: 'usa-accordion' } );
+
+	Array.from( accordions ).forEach( ( accordion ) => {
+		let heading = accordion.querySelector( 'h2, h3, h4, h5, h6' );
+		let content = accordion.querySelector( 'div:last-child' );
+		let accordionId = createId( heading.innerText );
+
+		// Create a new button for the heading
+		let button = domEl( 'button', {
+			type: 'button',
+			class: 'usa-accordion__button',
+			'aria-expanded': 'false',
+			'aria-controls': accordionId,
+		} );
+		button.innerText = heading.innerText;
+		heading.textContent = '';
+		heading.classList.add( 'usa-accordion__heading' );
+		heading.appendChild( button );
+
+		// Create new content div
+		let contentEl = domEl( 'div', { class: 'usa-accordion__content usa-prose', id: accordionId, 'hidden': 'true' } );
+		contentEl.appendChild( content );
+
+		// Append new elements
+		usaAccordion.appendChild( heading );
+		usaAccordion.appendChild( contentEl );
+	} );
+
+	block.textContent = '';
+	block.appendChild( usaAccordion );
 }
