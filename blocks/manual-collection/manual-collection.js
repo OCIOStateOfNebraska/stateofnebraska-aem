2104de4d281fadcc2216532760d51f07c83a9bd6
sbommakanti <sbommakanti@reingold.com>
2025-04-23 15:08:14 -0400

    f-88 -updated code review comments

--- blocks/manual-collection/manual-collection.js
+++ blocks/manual-collection/manual-collection.js
@@ -1,13 +1,16 @@
 import { domEl } from '../../scripts/dom-helpers.js';
+
 function getDateValue ( date ) {
 	return date + 'T00:00:00';
 }
+
 function getDateText ( date ) {
 	const dateObj = new Date( date );
 	const options = { year: 'numeric', month: 'long', day: 'numeric' };
 	const formattedDate = new Intl.DateTimeFormat( 'en-US', options ).format( dateObj );
 	return formattedDate;
 }
+
 export default function decorate( block ) {
 	const ul = domEl( 'ul', { class: 'usa-collection' } );
 	[...block.children].forEach( ( row ) => {
@@ -19,15 +22,20 @@ export default function decorate( block ) {
 		const anchor = h4.querySelector( 'a' );
 		anchor.classList.add( 'usa-link' );
 		while ( row.firstElementChild ) {
-			const para = row.firstElementChild;
+			const divBody = row.firstElementChild;
+			const para = divBody.querySelector( 'p' );
 			para.classList.add( 'usa-collection__description' );
-			li.append( para );
+			li.append( divBody );
 		}
 		let date = div.lastElementChild.innerHTML;
 		const timeTag = domEl( 'time', { 'datetime': getDateValue( date ) } );
 		timeTag.innerHTML = getDateText( date );
 		div.lastElementChild.remove();
-		div.appendChild( timeTag );
+		const metaUl = domEl( 'ul', { 'class': 'usa-collection__meta', 'aria-label': 'More information' } );
+		const metaLi = domEl( 'li', { 'class': 'usa-collection__meta-item' } );
+		metaLi.append( timeTag );
+		metaUl.append( metaLi );
+		div.appendChild( metaUl );
 		ul.append( li );
 	} );
 	block.textContent = '';
