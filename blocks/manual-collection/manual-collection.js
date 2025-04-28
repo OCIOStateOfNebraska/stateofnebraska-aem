358b83e6bec11a28377280e64dc538dc7030558d
sbommakanti <sbommakanti@reingold.com>
2025-04-28 11:55:52 -0400

    f-88 -updated code review suggestion for optional issue for itle , description and header aswell

--- blocks/manual-collection/manual-collection.js
+++ blocks/manual-collection/manual-collection.js
@@ -1,14 +1,39 @@
 import { domEl } from '../../scripts/dom-helpers.js';
 
-function getDateValue ( date ) {
+function getDateValue( date ) {
 	return date + 'T12:00:00';
 }
 
-function getDateText ( date ) {
+function getDateText( date ) {
 	const dateObj = new Date( date );
 	const options = { year: 'numeric', month: 'long', day: 'numeric' };
 	const formattedDate = new Intl.DateTimeFormat( 'en-US', options ).format( dateObj );
 	return formattedDate;
+}
+
+function setDate( div ) {
+	let date = div.lastElementChild.innerHTML;
+	const timeTag = document.createElement( 'time' );
+	timeTag.setAttribute( 'datetime', getDateValue( date ) );
+	timeTag.innerHTML = getDateText( getDateValue( date ) );
+	div.lastElementChild.remove();
+	const metaUl = document.createElement( 'ul' );
+	metaUl.classList.add( 'usa-collection__meta' );
+	metaUl.setAttribute( 'aria-label', 'More information' );
+	const metaLi = document.createElement( 'li' );
+	metaLi.classList.add( 'usa-collection__meta-item' );
+	metaLi.append( timeTag );
+	metaUl.append( metaLi );
+	div.appendChild( metaUl );
+}
+
+function setHeadingStyle( div ) {
+	if ( div.querySelector( 'h4' ) ) {
+		const h4 = div.querySelector( 'h4' );
+		h4.classList.add( 'usa-collection__heading' );
+		const anchor = h4.querySelector( 'a' );
+		anchor.classList.add( 'usa-link' );
+	}
 }
 
 export default function decorate( block ) {
@@ -24,28 +49,18 @@ export default function decorate( block ) {
 				div.remove();
 			} else if ( div.children.length > 1 ) {
 				div.classList.add( 'usa-collection__body' );
-				const h4 = div.querySelector( 'h4' );
-				h4.classList.add( 'usa-collection__heading' );
-				const anchor = h4.querySelector( 'a' );
-				anchor.classList.add( 'usa-link' );
-				const para = div.querySelector( 'p' );
-				para.classList.add( 'usa-collection__description' );
-				let date = div.lastElementChild.innerHTML;
-				const timeTag = document.createElement( 'time' );
-				timeTag.setAttribute( 'datetime', getDateValue( date ) );
-				timeTag.innerHTML = getDateText( getDateValue( date ) );
-				div.lastElementChild.remove();
-				const metaUl = document.createElement( 'ul' );
-				metaUl.classList.add( 'usa-collection__meta' );
-				metaUl.setAttribute( 'aria-label', 'More information' );
-				const metaLi = document.createElement( 'li' );
-				metaLi.classList.add( 'usa-collection__meta-item' );
-				metaLi.append( timeTag );
-				metaUl.append( metaLi );
-				div.appendChild( metaUl );
+				setHeadingStyle ( div );
+				div.querySelectorAll( 'p' ).forEach( ( ele ) => {
+					if ( new Date( ele.innerHTML ) instanceof Date && !isNaN( new Date( ele.innerHTML ) ) ) {
+						setDate( div );
+					} else {
+						const para = div.querySelector( 'p' );
+						para.classList.add( 'usa-collection__description' );
+					}
+				} );
 			}
 		} );
-		if ( li.querySelector( '.usa-collection__img' ) ) li.querySelector( '.usa-collection__img' ).setAttribute( 'alt', li.querySelector( '.usa-collection__heading > a' ).innerHTML );
+		if ( li.querySelector( '.usa-collection__img' ) && li.querySelector( '.usa-collection__heading > a' ) ) li.querySelector( '.usa-collection__img' ).setAttribute( 'alt', li.querySelector( '.usa-collection__heading > a' ).innerHTML );
 		ul.append( li );
 	} );
 	block.textContent = '';
