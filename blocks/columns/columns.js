908d9a945a826a0d9104bdd2b32706754a90f336
Carly Wooten <cwooten@reingold.com>
2025-05-01 12:23:06 -0700

    get columns outputting correctly

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,18 +1,17 @@
-export default function decorate( block ) {
-	const cols = [...block.firstElementChild.children];
-	block.classList.add( `columns-${cols.length}-cols` );
+import { domEl } from '../../scripts/dom-helpers.js';
 
-	// setup image columns
+
+export default function decorate( block ) {
+	const mainRow = domEl( 'div', {class: 'grid-row grid-gap'} );
+
+	// // setup image columns
 	[...block.children].forEach( ( row ) => {
 		[...row.children].forEach( ( col ) => {
-			const pic = col.querySelector( 'picture' );
-			if ( pic ) {
-				const picWrapper = pic.closest( 'div' );
-				if ( picWrapper && picWrapper.children.length === 1 ) {
-					// picture is only content in column
-					picWrapper.classList.add( 'columns-img-col' );
-				}
-			}
+			col.classList.add( 'grid-col-12', 'desktop:grid-col-6' );
+			mainRow.append( col );
 		} );
 	} );
+	
+	block.innerText = '';
+	block.append( mainRow );
 }
