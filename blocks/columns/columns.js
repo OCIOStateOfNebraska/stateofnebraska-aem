3dad4ace066cd56d4d1895f3886b3bd829564097
Carly Wooten <cwooten@reingold.com>
2025-05-01 15:59:02 -0700

    update column spacing. Fix back to top styling

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -3,11 +3,11 @@ import { domEl } from '../../scripts/dom-helpers.js';
 
 export default function decorate( block ) {
 	// append to one main Row just in case they author the wrong number of columns. This way we don't have any gaps 
-	const mainRow = domEl( 'div', {class: 'grid-row grid-gap'} );
+	const mainRow = domEl( 'div', {class: 'columns__grid-row grid-row grid-gap'} );
 
 	[...block.children].forEach( ( row ) => {
 		[...row.children].forEach( ( col ) => {
-			col.classList.add( 'grid-col-12', 'desktop:grid-col-6' );
+			col.classList.add( 'columns__grid-col', 'grid-col-12', 'desktop:grid-col-6' );
 			mainRow.append( col );
 		} );
 	} );
