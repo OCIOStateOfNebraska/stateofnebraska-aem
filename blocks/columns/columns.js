0800c19fbd416e8160ab72df253206329bc2a7be
Carly Wooten <cwooten@reingold.com>
2025-05-01 12:23:49 -0700

    add comment

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,9 +2,9 @@ import { domEl } from '../../scripts/dom-helpers.js';
 
 
 export default function decorate( block ) {
+	// append to one main Row just in case they author the wrong number of columns. This way we don't have any gaps 
 	const mainRow = domEl( 'div', {class: 'grid-row grid-gap'} );
 
-	// // setup image columns
 	[...block.children].forEach( ( row ) => {
 		[...row.children].forEach( ( col ) => {
 			col.classList.add( 'grid-col-12', 'desktop:grid-col-6' );
