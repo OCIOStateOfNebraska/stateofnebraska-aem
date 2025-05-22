84cd60e46c05b15730064ef325abaa0c31e6369e
Kevin Harris <kharris@reingold.com>
2025-05-22 15:34:05 -0400

    comment

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -447,7 +447,7 @@ await loadPage();
 	setTimeout( addClass, 2000 );
 } )();
 
-// document authoring snippet
+// enable document authoring snippet
 ( async function loadDa() {
 	if ( !new URL( window.location.href ).searchParams.get( 'dapreview' ) ) return;
 	import( 'https://da.live/scripts/dapreview.js' ).then( ( { default: daPreview } ) => daPreview( loadPage ) );
