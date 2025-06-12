1d6fbbed11fa2711f9b961518996e669baa2cf14
sbommakanti <sbommakanti@reingold.com>
2025-06-12 10:58:32 -0400

    f-146: missing semi-colon.

--- tools/importer/import-ndbf.js
+++ tools/importer/import-ndbf.js
@@ -268,7 +268,7 @@ const createMetadataBlock = ( main, document, url ) => {
 const formatDate = ( date ) => {
 	return new Intl.DateTimeFormat( 'en-US', { month: 'long', day: 'numeric', year: 'numeric'} ).format( date ) +
 	' - ' + new Intl.DateTimeFormat( 'en-US', { hour: 'numeric', minute: '2-digit', hour12: true} ).format( date ).toLowerCase();
-}
+};
 
 export default {
 	transform: ( {
