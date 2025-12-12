7bbaef8b8b4af11df3b67791029a769c0f8f24f0
Oleksandr Khokhulia <Oleksandr.khokhulia@nebraska.gov>
2025-12-12 10:41:18 -0600

    in-page-nav fix

--- blocks/in-page-navigation/in-page-navigation.js
+++ blocks/in-page-navigation/in-page-navigation.js
@@ -12,43 +12,6 @@ export default async function decorate( block ) {
 		'data-main-content-selector': '.main-content',
 	} );
 
-	// stick nav to top on mobile and scroll to current item
-	function mobileNavCurrent() {
-		const px = parseFloat( getComputedStyle( document.documentElement ).fontSize ) ;
-		const nav = document.querySelector( '.usa-in-page-nav' );
-		const navList = nav.querySelector( '.usa-in-page-nav__list' );
-		const navTop = document.querySelector( '.in-page-navigation.block' ).getBoundingClientRect().top + window.scrollY;
-		if ( window.scrollY > navTop && window.innerWidth < px * 40 && window.matchMedia( '(orientation: portrait)' ).matches ) {
-			nav.style.position = 'fixed';
-			nav.style.top = '0';
-			nav.style.left = '0';
-			nav.style.right = '0';
-			nav.querySelector( '.usa-in-page-nav__heading' ).style.display= 'none';
-
-			
-			nav.style.zIndex= 10;
-			nav.style.isolation= 'isolate';
-			setTimeout( () => {
-				const currentLink = navList.querySelector( '.usa-current' );
-				if ( currentLink ) {
-					navList.scrollTo( {
-						left: currentLink.parentElement.offsetLeft,
-					} );
-					nav.style.backgroundColor = 'white';
-				}
-
-			}, 100 );
-		}
-		else {
-			nav.style.position = 'static';
-			nav.style.backgroundColor = 'transparent';
-			nav.querySelector( '.usa-in-page-nav__heading' ).style.display= 'block';
-		}
-	}
-
-	document.addEventListener( 'scroll', mobileNavCurrent );
-	sidenav.addEventListener( 'click', mobileNavCurrent );
-
 	block.textContent = '';
 	block.appendChild( sidenav );
 
