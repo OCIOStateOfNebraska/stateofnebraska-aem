a29d26b907c392bc3239b6cf6638dd3bc9b7786d
Oleksandr Khokhulia <Oleksandr.khokhulia@nebraska.gov>
2026-04-21 07:58:08 -0500

    Showcase functionality added

--- blocks/gallery/gallery.js
+++ blocks/gallery/gallery.js
@@ -1,16 +1,63 @@
 import { domEl } from '../../scripts/dom-helpers.js';
+import {  getIndividualIcon } from '../../scripts/utils.js';
+
+function showcasePicture( e, modal, thumbnailContainer){
+	modal.classList.remove('hidden');
+	const figure =  modal.querySelector( 'figure' );
+	if(	figure ){
+		thumbnailContainer.append( figure );
+	}
+	modal.prepend( e.currentTarget );
+	e.currentTarget.focus();
+	// rows.forEach( row => row.classList.remove( 'showcased' ) );
+	// item.classList.add( 'showcased' );
+	
+	// const img = item.querySelector( 'img' );
+	// console.log(img.getAttribute('width'))	
+}
+
 
 export default function decorate( block ) {
 	const rows = Array.from( block.children );
+	const thumbnailContainer = domEl( 'div', { class: 'thumbnail__container' }); 
+
+	const modal = domEl( 'div', { class: 'gallery__modal hidden', role: 'dialog', ['aria-modal']: true, ['aria-labelledby']: 'gallery-caption' } ); 
+	const modalButton  = domEl( 'button', { class: 'usa-button usa-button--outline', ['aria-label']:'Close image'} );
+	// getIndividualIcon( modalButton, 'expand_more' );
+	modalButton.addEventListener( 'click', () => modal.classList.add( 'hidden' ) )
+	modalButton.textContent = 'Hide Image';
+
 	rows.forEach( row => {
 		let count = 1; 
-		row.classList.add( 'gallery__item' );
+		// row.classList.add( 'gallery__item' );
 		const figure = domEl( 'figure', { class: 'gallery__media' } );    
+		const button  = domEl( 'button', { class: 'gallery__thumbnail' } );    
+		figure.addEventListener( 'click', (e) => showcasePicture(e, modal, thumbnailContainer) );
+
 		const img = row.querySelector( 'picture' );
 		const caption = row.querySelector( 'p' );
 		const link = row.querySelector( 'a' );
 
-		figure.append( img );
+		button.append( img );
+		figure.append( button );
+
+		const width = Number( figure.querySelector( 'img' )?.getAttribute( 'width' ) );
+		const height = Number( figure.querySelector( 'img' )?.getAttribute( 'height' ) );
+
+		// console.log(width)
+		// console.log(height)	
+
+		// if( width > height ){
+		// 	row.classList.add( 'horizontal' );
+
+		// }
+		// if( width == height ){
+		// 	row.classList.add( 'square' );
+
+		// }
+		// else {
+		// 	row.classList.add( 'portrait' );
+		// }
 		
 		if( caption ){
 			const figcaption = document.createElement( 'figcaption' );        
@@ -25,14 +72,24 @@ export default function decorate( block ) {
 			count ++;
 		}
 
-		row.append( figure );
+		thumbnailContainer.append( figure );
 		
 		if( link ){
 			link.classList.add( 'gallery__link' );
-			row.append( link );
+			thumbnailContainer.append( link );
 		}
 		
 		const divs = Array.from( row.querySelectorAll( 'div' ) );
-		divs.forEach( div => div.remove() );			
+		divs.forEach( div => div.remove() );	
+
+		row.remove()
+		
 	} );
+	
+	
+
+
+	modal.append( modalButton );
+	block.append( modal );
+	block.append( thumbnailContainer );
 }
